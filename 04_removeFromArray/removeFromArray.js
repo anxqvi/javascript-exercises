3d7ef29e607b30ea2b16c59b0c75de030337899e
anxqvi <133358334+anxqvi@users.noreply.github.com>
2024-05-26 23:52:31 +0400

    Completed exercises 1 to 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!targets.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
